96641502af659c6abe55e44e043b8487aae81acf
Benjamin Norwood <benjamin.norwood@cgifederal.com>
2017-03-30 21:40:05 -0500

    Removed Domain WIP for successful build.

--- src/domains/lafayette-art/art.js
+++ src/domains/lafayette-art/art.js
@@ -1,23 +1,4 @@
 (()=>{
-    const request = require("request");
     
-    const nconf   = require('nconf');
-    
-    const prepareRequest = function(){
-        
-    };
-    
-    module.exports = function(opts){
-        
-        return {
-            initialize: function(){
-                
-            },
-            prepareResponse: function(message){
-                return {
-                }
-            }
-        };
-    };
    
 })();
