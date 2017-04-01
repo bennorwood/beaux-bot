7b64f655ee3cd37cd525cf606dce85a3b00e68d0
Benjamin Norwood <benjamin.norwood@cgifederal.com>
2017-03-31 23:02:03 -0500

    JSHint update.

--- src/modules/geo-utils/geo-utils.js
+++ src/modules/geo-utils/geo-utils.js
@@ -3,7 +3,6 @@
  */
 (() => {
     
-    const nconf = require('nconf');
     const BlueBird = require('bluebird');
     
     const Geoservices = require('geoservices');
@@ -11,18 +10,17 @@
     BlueBird.promisifyAll(client.geocode);
     
     let lib = {
-        initialize: function(opts) {/*no-op*/
-            
+        initialize: function() {/*no-op*/
+            /**
+             * Leaving sample query setup below.
+             * https://github.com/Esri/geoservices-js/blob/master/docs/FeatureServices.md
+             */
+             /* 
+            let params = {
+                url: 'https://services.arcgis.com/xQcS4egPbZO43gZi/arcgis/rest/services/Lafayette_Public_Art/FeatureServer/0/'
+            };
+                
             this.getFeatureServiceClient(params).then((featureClient)=>{
-                /**
-                 * Leaving sample query setup below.
-                 * https://github.com/Esri/geoservices-js/blob/master/docs/FeatureServices.md
-                 */
-                 
-                /* 
-                let params = {
-                    url: 'https://services.arcgis.com/xQcS4egPbZO43gZi/arcgis/rest/services/Lafayette_Public_Art/FeatureServer/0/'
-                };
                 const queryParams = {
                     geometryType: 'esriGeometryPoint',
                     geometry: JSON.stringify({
@@ -37,25 +35,25 @@
                     outFields: '*',
                     returnGeometry: true,
                     units: 'esriSRUnit_Meter'
-                }; */
+                };
                 
-                /* featureClient.queryAsync(queryParams).then((data)=>{
+                featureClient.queryAsync(queryParams).then((data)=>{
                     console.log(data);
-                }); */
+                });
                 
-            });
+            });*/
         },
         ArcGISClient: client,
         getFeatureServiceClient: function(params){
             return new Promise( (resolve, reject) => {
-               let featureServiceClient = client.featureservice( params , function (err, result) {
+                let featureServiceClient = client.featureservice( params , function (err, result) {
                     if (err) {
-                        console.error("GEO-UTILS ERROR: " + err);
+                        console.error('GEO-UTILS ERROR: ' + err);
                         reject(err);
                     } else {
                         //promisify featureServiceClient
                         BlueBird.promisifyAll(featureServiceClient);
-                        console.log("Metadata: ");
+                        console.log('Metadata: ');
                         console.log(result);
                         resolve(featureServiceClient);
                     }
