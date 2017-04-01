2f0f4a0dbc841712de026513df015a58749f37c1
Alex Fontenot <guiri.alien@gmail.com>
2017-03-31 19:40:06 -0500

    Corrects the slack client to use lowercase "botkit"

--- src/clients/slack/slack.client.js
+++ src/clients/slack/slack.client.js
@@ -3,26 +3,26 @@
  *
  * To properly run client update the following environment variable:
  * 	SET SLACKBOT_CLIENT_ACCESS_TOKEN <your access token>
- * 
+ *
  */
 (() => {
     const path = require('path');
-    
-    const BotKit = require('botKit');
+
+    const BotKit = require('botkit');
     const nconf  = require('nconf');
     const blue    = require('chalk').blue;
     const red    = require('chalk').red;
     //If server in development mode add environment variable prefix to run separate instance of a slackbot
     const tokenName = nconf.get('resolveTokenKey')(nconf, nconf.get('mode'), 'SLACKBOT_CLIENT_ACCESS_TOKEN');
-    
+
     const apiaiManager = require( path.join( nconf.get('paths:modulesDir'), 'apiai', 'apiai.manager') );
     const responseManager = require( path.join( nconf.get('paths:modulesDir'), 'domain-requests', 'domain-request.manager') );
-    
+
     const CLIENT_NAME = nconf.get('clients:slack:name');
-    
+
     module.exports = function(opts){
         let slackBotController = null;
-        
+
         return {
             initialize: function(){
                 if(nconf.get('clients:slack:enabled') === true){
@@ -44,18 +44,18 @@
                 if(opts.botEvents){
                     //Need to add event handler method for all events
                     opts.botEvents.forEach(function(event){
-                        slackBotController.on(event, function(bot, message){                            
+                        slackBotController.on(event, function(bot, message){
                             apiaiManager.processUserMessage(bot, message).then(()=>{
                                 console.log(message);
                             }, console.log)
                             .then(()=>{
                                 return responseManager.responseDigest(message).then((responseMethods)=> {
                                     responseManager.respond(CLIENT_NAME, responseMethods, bot, message);
-                                }); 
+                                });
                             });
                         });
                     });
-                    
+
                 } else {
                     console.log(red('No event handlers specified for slack bot...'));
                 }
